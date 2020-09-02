6fff08d3958e06c5f4fc8fbcf3282d581b1f545f
keerthanavelan <67827550+keerthanavelan@users.noreply.github.com>
2020-09-02 20:59:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var dogImg, happyDog, database, foodS, foodStock, dog;
+// 
+function preload()
+{
+  dogImg = loadImage('Dog.png');
+  happyDog = loadImage('happydog.png');
+}
+
+function setup() {
+  createCanvas(500, 500);
+  dog = createSprite(250,250)
+  dog.addImage(dogImg)
+  dog.scale = 0.3
+  database = firebase.database;
+  console.log("Is there a problem in sketch.js:16?")
+  foodStock = database.ref('pet/food');
+  console.log("NP");
+  foodStock.on("value", readStock)
+  
+}
+
+function draw() {  
+  background(64, 224, 208)
+  drawSprites();
+  //add styles here
+  if (keyWentUp(UP_ARROW)){
+    //writeStock(foodS);
+    dog.addImage(happyDog);
+
+  }
+  readStock();
+  writeStock();
+}
+
+function readStock(data){
+  foodS = data.val();
+}
+
+function writeStock(x){
+  database.ref('/').update({
+    Food : x
+  })
+}
